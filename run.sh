7629b7d308b0b95ee288f03ed59098339a49d302
guo_22084 <yuejun.guo@list.lu>
2024-08-08 18:14:35 +0200

    [Fix] errors

--- run.sh
+++ run.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-tag_models=("mrm8488/codebert-base-finetuned-detect-insecure-code" "claudios/VulBERTa-MLP-ReVeal")
-tgt_datasets=("Zaib/java-vulnerability" "google/code_x_glue_cc_defect_detection")
+tgt_models=("mrm8488/codebert-base-finetuned-detect-insecure-code" "claudios/VulBERTa-MLP-ReVeal")
 attcks=("greedy" "ga" "codefooler" "codeattack" "alert" "mhm")
 max_iter=200
 top_k=60
 
-declare -A datasets_lang_map=(
-    ["mrm8488/codebert-base-finetuned-detect-insecure-code"]="java"
-    ["claudios/VulBERTa-MLP-ReVeal"]="c"
-)
 
 declare -A atk_params_map
 atk_params_map["greedy"]="--max_iter $max_iter --top_k $top_k"
@@ -21,16 +16,22 @@ atk_params_map["mhm"]="--max_iter $max_iter --top_k $top_k --n_candi 60 --prob_t
 
 
 
-for tag_model in "${tag_models[@]}"
+for tgt_model in "${tgt_models[@]}"
 do
     for atk in "${attcks[@]}"
     do
         params=${atk_params_map[$atk]}
-        for tgt_dataset in "${tgt_datasets[@]}"
-        do
-            lang=${datasets_lang_map[$tgt_dataset]}
-            python test.py --max_iter $max_iter --top_k $top_k --tgt_model $tgt_model --test_dataset $tgt_dataset --data_lang $lang --attack $atk $params || continue
-            echo "python test.py --max_iter $max_iter --top_k $top_k --tgt_model $tgt_model --test_dataset $tgt_dataset --data_lang $lang --attack $atk $params" || continue
-        done
+        echo "python test.py --max_iter $max_iter --top_k $top_k --tgt_model $tgt_model --test_dataset Zaib/java-vulnerability --data_lang java --attack $atk $params" || continue
+        python test.py --max_iter $max_iter --top_k $top_k --tgt_model $tgt_model --test_dataset Zaib/java-vulnerability --data_lang java --attack $atk $params || continue
+    done
+done
+
+for tgt_model in "${tgt_models[@]}"
+do
+    for atk in "${attcks[@]}"
+    do
+        params=${atk_params_map[$atk]}
+        echo "python test.py --max_iter $max_iter --top_k $top_k --tgt_model $tgt_model --test_dataset google/code_x_glue_cc_defect_detection --data_lang c --attack $atk $params" || continue
+        python test.py --max_iter $max_iter --top_k $top_k --tgt_model $tgt_model --test_dataset google/code_x_glue_cc_defect_detection --data_lang c --attack $atk $params || continue
     done
 done
